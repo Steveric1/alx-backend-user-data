7711ee45c9609f2ea81669713a03ce1777150a14
Eric <stephen.erich243@gmail.com>
2024-06-13 17:08:44 +0000

    user authentication

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -3,13 +3,54 @@
 Definition of _hash_password function
 """
 import bcrypt
+from sqlalchemy.orm.exc import NoResultFound
+from db import DB
+from user import User
 
 
 def _hash_password(password: str) -> bytes:
     """
-    Hashes a password string and returns it in bytes form
+    _hash_password - method that takes in a password string arguments
+    and return bytes.
     Args:
-        password (str): password in string format
+       - password(str): password to encrypt
+    Return:
+        bytes
     """
-    passwd = password.encode('utf-8')
-    return bcrypt.hashpw(passwd, bcrypt.gensalt())
+    hashed_password = password.encode('utf-8')
+    return bcrypt.hashpw(hashed_password, bcrypt.gensalt())
+
+
+class Auth:
+    """Auth class to interact with the authentication database
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """
+        Method that take mandatory email and password string arguments
+        and return a User object.
+        Args:
+           - email(str): user's email
+           - password(str): user's password
+
+        Return:
+            user object
+        Raises:
+            - ValueError: if user already exist with passed email.
+        """
+        try:
+            usr = self._db.find_user_by(email=email)
+            if usr.email == email:
+                raise ValueError("User {} already exists".format(email))
+        except NoResultFound:
+            passwd = _hash_password(password)
+            user = User(email=email, hashed_password=passwd)
+            session = self._db._session
+            session.add(user)
+            session.commit()
+            return user
+        except ValueError:
+            raise
