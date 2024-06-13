81576459752f60fd107bdb67dd72650c81076485
Eric <stephen.erich243@gmail.com>
2024-06-13 05:32:18 +0000

    user authentication

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 """
-task you will create a SQLAlchemy model named User for a 
+task you will create a SQLAlchemy model named User for a
 database table named users (by using the mapping declaration of SQLAlchemy).
 """
 
