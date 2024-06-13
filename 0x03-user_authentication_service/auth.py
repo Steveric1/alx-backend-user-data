bceea9f77f6c462be042df37b145335c48dedbdf
Eric <stephen.erich243@gmail.com>
2024-06-13 06:15:59 +0000

    user authentication

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -7,12 +7,9 @@ import bcrypt
 
 def _hash_password(password: str) -> bytes:
     """
-    _hash_password - method that takes in a password string arguments
-    and return bytes.
+    Hashes a password string and returns it in bytes form
     Args:
-       - password(str): password to encrypt
-    Return:
-        bytes
+        password (str): password in string format
     """
-    hashed_password = password.encode('utf-8')
-    return bcrypt.hashpw(hashed_password, bcrypt.gensalt())
+    passwd = password.encode('utf-8')
+    return bcrypt.hashpw(passwd, bcrypt.gensalt())
