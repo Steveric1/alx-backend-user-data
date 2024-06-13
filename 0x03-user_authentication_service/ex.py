cc4a89446828dd46264dd3c7bde4ba8aefc6c391
Eric <stephen.erich243@gmail.com>
2024-06-13 01:42:32 +0000

    user authentication

---
+++ 0x03-user_authentication_service/ex.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+from flask import Flask
+
+app = Flask(__name__)
+
+print(app)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+@app.route('/projects/')
+def projects():
+    return 'The project page'
+
+@app.route('/about')
+def about():
+    return 'The about page'
+
+if __name__ == '__main__':
+    app.run()
