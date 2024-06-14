c80095f7fe50994bad80975934ed0084aea5b3d7
Eric <stephen.erich243@gmail.com>
2024-06-14 23:03:03 +0000

    user authentication

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -110,5 +110,10 @@ class Auth:
         """
         if session_id is None:
             return None
-        user = self._db.find_user_by(session_id=session_id)
+
+        try:
+            user = self._db.find_user_by(session_id=session_id)
+        except NoResultFound:
+            return None
+
         return user
