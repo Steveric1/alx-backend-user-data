4d2ece8c5dacfa39208b56dd9ae031cfaf9014d2
Eric <stephen.erich243@gmail.com>
2024-06-14 23:34:34 +0000

    user authentication

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 """Set up a basic flask app
 """
-from flask import Flask, jsonify, request, abort, make_response
+from flask import (Flask, jsonify, request,
+                   abort, redirect, url_for)
 from user import User
 from auth import Auth
 
@@ -47,6 +48,24 @@ def login() -> str:
     response.set_cookie("session_d", session_id)
 
     return response
+
+
+@app.route("/sessions", methods=["DELETE"], strict_slashes=False)
+def logout() -> str:
+    """logout function implementation"""
+    session_id = request.cookies.get("session_id", None)
+
+    if session_id is None:
+        abort(403)
+    try:
+        user = AUTH._db.find_user_by(session_id=session_id)
+        if user:
+            AUTH.destroy_session(user.id)
+            return redirect("/")
+        else:
+            abort(403)
+    except Exception as e:
+        abort(403)
 
 
 if __name__ == "__main__":
