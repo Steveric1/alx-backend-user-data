0842dc2952da2a1d4256c070ed9fb28528085156
Eric <stephen.erich243@gmail.com>
2024-06-14 23:00:58 +0000

    user authentication

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -7,6 +7,7 @@ from sqlalchemy.orm.exc import NoResultFound
 from db import DB
 from user import User
 from uuid import uuid4
+from typing import Optional
 
 
 def _hash_password(password: str) -> bytes:
@@ -97,3 +98,17 @@ class Auth:
             return session_id
         except NoResultFound:
             return None
+
+    def get_user_from_session_id(self, session_id: str) -> Optional[User]:
+        """
+        Method to implement the get user from session id
+        Args:
+            - session_id(str): session id
+
+        Return:
+            corresponding User or None
+        """
+        if session_id is None:
+            return None
+        user = self._db.find_user_by(session_id=session_id)
+        return user
