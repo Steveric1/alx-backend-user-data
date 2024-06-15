8e312bef0e278c6d42673af3127ccd36b6ab74c7
Eric <stephen.erich243@gmail.com>
2024-06-15 00:37:53 +0000

    user authentication

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -72,5 +72,17 @@ def profile() -> str:
     abort(403)
 
 
+@app.route("/reset_password", methods=["POST"], strict_slashes=False)
+def get_reset_password_token() -> str:
+    """route to get reset password token"""
+    email = request.form.get("email")
+
+    user = AUTH._db.find_user_by(email=email)
+    if user is None:
+        abort(403)
+    token = AUTH.get_reset_password_token(email)
+    return jsonify({"email": f"{user.email}", "reset_token": f"{token}"}), 200
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
